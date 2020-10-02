a70a38f9bb1b460a2eb631bd4375f1d1f0bf92ec
raraspradnya <13518013@std.stei.itb.ac.id>
2020-10-03 00:56:28 +0700

    draw board an pawn

--- src/gui.py
+++ src/gui.py
@@ -24,6 +24,8 @@ class BoardGUI(tk.Tk):
 
             col_label = tk.Label(self, text=chr(i+97), font='Times', bg='#fff', fg='#000')
             col_label.grid(row=0, column=i+1)
+        
+        self.tiles = {}
 
         # grid canvas
         # showing white blank canvas with size 600*600
@@ -34,8 +36,91 @@ class BoardGUI(tk.Tk):
         self.rowconfigure(0, minsize=50)
         self.columnconfigure(self.board_size + 1, minsize=50)
         self.rowconfigure(self.board_size + 1, minsize=50)
+        self.canvas.bind("<Configure>", self.drawTiles)
+    
+    def drawTiles(self, event=None):
+        self.canvas.delete("tile")
+        canvas_width = 600
+        canvas_height = 600
+        border_size = 1
+        cell = int(canvas_height / self.board_size)
+        for col in range (self.board_size):
+            for row in range (self.board_size):
+                x1 = col * cell + border_size / 2
+                y1 = row * cell + border_size / 2
+                x2 = (col + 1) * cell - border_size / 2
+                y2 = (row + 1) * cell - border_size / 2
+
+                if (self.board_size == 8):
+                    player1 = 4
+                    player2 = 10
+                elif (self.board_size == 10):
+                    player1 = 5
+                    player2 = 13
+                else:
+                    player1 = 6
+                    player2 = 24
+
+                if ((row + col) < player1):
+                    if ((row + col) % 2 == 0):
+                        color = '#AC352E'
+                    else:
+                        color = '#D0352E'
+                elif ((row + col ) > player2):
+                    if ((row + col) % 2 == 0):
+                        color = '#12C47A'
+                    else:
+                        color = '#0FA868'
+                else:
+                    if ((row + col) % 2 == 0):
+                        color = '#ECCB96'
+                    else:
+                        color = '#BAA077'
+                tile = self.canvas.create_rectangle(x1, y1, x2, y2, tags="tile", width=0, fill=color)
+                self.tiles[row,col] = tile
+                self.canvas.tag_bind(tile, "<1>", lambda event, row=row, col=col: self.clicked(row, col))
+
+            self.drawPawn()
+
+    def clicked(self, row, column):
+        #TODO yang ada pionnya blm bisa diclick
+        tile = self.tiles[row,column]
+        border = int(float(self.canvas.itemcget(tile, "width")))
+        if border == 0:
+            border = 2
+        else:
+            border = 0
+        self.canvas.itemconfigure(tile, outline="black", width = border)
+
+    def drawPawn(self):
+        canvas_width = 600
+        canvas_height = 600
+        border_size = 10
+        cell = int(canvas_height / self.board_size)
+        player1Pawn = self.board.player1.pawns
+        player2Pawn = self.board.player2.pawns
+
+        for i in range(len(player1Pawn)):
+            col = player1Pawn[i].x - 1
+            row = player1Pawn[i].y -1 
+            x1 = col * cell + border_size / 2
+            y1 = row * cell + border_size / 2
+            x2 = (col + 1) * cell - border_size / 2
+            y2 = (row + 1) * cell - border_size / 2
+            pawn = self.canvas.create_oval(x1, y1, x2, y2, tags="pawn", width=0, fill="#CF6E67")
+
+        for i in range(len(player2Pawn)):
+            col = self.board_size - player2Pawn[i].x
+            row = self.board_size - player2Pawn[i].y
+            x1 = col * cell + border_size / 2
+            y1 = row * cell + border_size / 2
+            x2 = (col + 1) * cell - border_size / 2
+            y2 = (row + 1) * cell - border_size / 2
+            pawn = self.canvas.create_oval(x1, y1, x2, y2, tags="pawn", width=0, fill="#67BF9B")
+        self.canvas.tag_bind(pawn, "<1>", lambda event, row=row, col=col: self.clicked(row, col))
+
 
 if __name__ == '__main__':
-    board = Board(16,3)
+    board = Board(8,3)
     gui = BoardGUI(board)
     gui.mainloop()
